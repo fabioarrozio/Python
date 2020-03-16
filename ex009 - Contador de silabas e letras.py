fa24403651b5a6864b7a81a38ed71f55fedb43d6
Fabio Arrozio <52324328+fabioarrozio@users.noreply.github.com>
2020-03-16 11:10:23 -0300

    Add files via upload

---
+++ ex009 - Contador de silabas e letras.py
@@ -0,0 +1,33 @@
+count = 0
+countv = 0
+
+while True:
+    p = input("Digite uma palavra: ")
+    count += 1 #contador de palavras
+    
+    #verificar se começa com vogal
+    ini = p[0]
+    if ini == 'a' or ini == 'e' or ini == 'i' or ini == 'o' or ini == 'u':
+        countv += 1 #contador de vogal
+   
+    tam = len(p)
+    print("A palavra {} tem {} letras".format(p, tam))
+    for x in range (tam-1):
+        print(p[x], '-', end='') #escreve letras separadas por hifen
+   
+    vogais = 0
+    for y in range (tam-1): #contador de vogais na palavra digitada
+        if p[y] == 'a' or p[y] == 'e' or p[y] == 'i' or p[y] == 'o' or p[y] == 'u':
+            vogais += 1
+    
+    #sem tratamento para letra maiuscula e acentos.
+    print(p[-1]) # pega a ultima letra
+    print(p, "ao contrário é: ", p[::-1])
+    print("\n{} possui {} vogais".format(p, vogais+1)) 
+    
+    resp = str(input("Deseja digitar outra palavra? (S/N): "))
+    if resp.upper() != "S":
+        break
+   
+    print("\nTotal de palavras: {}".format(count))
+    print("Palavras iniciadas por vogal: {}".format(countv))
